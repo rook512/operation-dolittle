d526ff1e202a26d90bd5d7705eabad15b6d42c22
rook512 <rook512@gmail.com>
2017-10-18 19:23:52 -0500

    Updated ship location drawing

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,6 +1,6 @@
-import { drawPlayerBullet } from "./visuals/playerBullet";
+//import { drawPlayerBullet } from "./visuals/playerBullet";
 import { drawPlayerShip } from "./visuals/playerShip";
-import { drawLaser } from "./visuals/laser";
+//import { drawLaser } from "./visuals/laser";
 
 const canvas = document.querySelector("canvas");
 if (canvas === null) {
@@ -14,6 +14,11 @@ const width = canvas.width;
 const height = canvas.height;
 context.clearRect(0, 0, width, height);
 
-drawPlayerBullet(400, 300, context);
-drawLaser(300, 300, context);
-drawPlayerShip(200, 450, context);
+//drawPlayerBullet(400, 300, context);
+context.save();
+context.translate(200, 200);
+context.rotate(0);
+context.scale(1, 1);
+drawPlayerShip(context);
+context.restore();
+//drawLaser(300, 300, context);
--- scripts/visuals/playerShip.ts
+++ scripts/visuals/playerShip.ts
@@ -1,39 +1,35 @@
-export function drawPlayerShip(
-  cx: number,
-  cy: number,
-  context: CanvasRenderingContext2D
-) {
+export function drawPlayerShip(context: CanvasRenderingContext2D) {
   context.beginPath();
-  context.moveTo(cx + 4, cy + 14);
-  context.quadraticCurveTo(cx + 8, cy - 14, cx + 13, cy + 14);
-  context.quadraticCurveTo(cx + 8, cy + 14, cx + 4, cy + 14);
-  context.quadraticCurveTo(cx + 8, cy + 16, cx + 13, cy + 14);
+  context.moveTo(+4, +14);
+  context.quadraticCurveTo(+8, -14, +13, +14);
+  context.quadraticCurveTo(+8, +14, +4, +14);
+  context.quadraticCurveTo(+8, +16, +13, +14);
   context.fillStyle = "#FFFFFF";
   context.fill();
   context.strokeStyle = "#000000";
   context.stroke();
   context.beginPath();
-  context.moveTo(cx - 4, cy + 14);
-  context.quadraticCurveTo(cx - 8, cy - 14, cx - 13, cy + 14);
-  context.quadraticCurveTo(cx - 8, cy + 14, cx - 4, cy + 14);
-  context.quadraticCurveTo(cx - 8, cy + 16, cx - 13, cy + 14);
+  context.moveTo(-4, +14);
+  context.quadraticCurveTo(-8, -14, -13, +14);
+  context.quadraticCurveTo(-8, +14, -4, +14);
+  context.quadraticCurveTo(-8, +16, -13, +14);
   context.fillStyle = "#FFFFFF";
   context.fill();
   context.strokeStyle = "#000000";
   context.stroke();
   context.beginPath();
-  context.moveTo(cx + 4, cy - 1.2);
-  context.lineTo(cx + 10, cy - 6);
-  context.lineTo(cx + 8, cy - 14);
-  context.lineTo(cx + 10, cy - 16);
-  context.lineTo(cx + 16, cy + 2);
-  context.lineTo(cx, cy + 16);
-  context.lineTo(cx - 16, cy + 2);
-  context.lineTo(cx - 10, cy - 16);
-  context.lineTo(cx - 8, cy - 14);
-  context.lineTo(cx - 10, cy - 6);
-  context.lineTo(cx - 4, cy - 1.2);
-  context.quadraticCurveTo(cx, cy - 24, cx + 4, cy - 1.2);
+  context.moveTo(+4, -1.2);
+  context.lineTo(+10, -6);
+  context.lineTo(+8, -14);
+  context.lineTo(+10, -16);
+  context.lineTo(+16, +2);
+  context.lineTo(+0, +16);
+  context.lineTo(-16, +2);
+  context.lineTo(-10, -16);
+  context.lineTo(-8, -14);
+  context.lineTo(-10, -6);
+  context.lineTo(-4, -1.2);
+  context.quadraticCurveTo(+0, -24, +4, -1.2);
   context.closePath();
   context.fillStyle = "#FFFFFF";
   context.fill();
